728108ad0e81cb506c7394378a2039abb7d9f874
jemiliogzz <122490063+jemiliogzz@users.noreply.github.com>
2025-09-23 20:46:43 -0600

    Create quiz_9.py

---
+++ pages/quiz_9.py
@@ -0,0 +1,214 @@
+#Reutilizable
+import streamlit as st
+import random
+import time 
+from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col
+
+if "mat" in st.session_state:
+    mat = st.session_state["mat"]
+else:
+    st.switch_page("streamlit_app.py")
+
+#st.write(st.session_state.tema)
+cnx = st.connection("snowflake")
+session = cnx.session()
+info = session.table("primeroc.public.subjects").filter(col('id_tema')==st.session_state.tema).collect()[0]
+st.title(info[1])
+st.write("Dificultad:", info[2])
+
+new_seed = random.randint(1, 10000)
+
+random.seed(st.session_state.s_seed)
+
+# Initialize session state variable
+if 'button_disabled' not in st.session_state:
+    st.session_state.button_disabled = False
+
+# Callback function to disable the button
+def disable_button():
+    st.session_state.button_disabled = True
+
+preguntas = []
+respuestas = []
+
+for i in range (5):
+    #Fin
+
+    res = ['Natural', 'Entero', 'Racional', 'Real']
+
+    num = random.randint(1, 4 + i)
+
+    sig = random.choice(['sip', 'nop'])
+
+    if sig == 'sip':
+        num  *= -1
+        res.remove('Natural')
+        sig = random.choice(['ent', 'rac', 'irr'])
+        if sig == 'rac':
+            res.remove('Entero')
+            num1 = random.randint(-4, 5 + i)
+            if num1 == 0:
+                num1 = 1
+
+            if num % num1 == 0:
+                res.append('Entero')
+                if num1 < 0:
+                    res.append('Natural')
+                latex_str = str(int(num / num1))
+            else: 
+                disp = random.choice(['\div', 'num'])
+                if disp == '\div':
+                    latex_str = str(num) + disp + str(num1)
+                else:
+                    num = num / num1
+                    latex_str = str(round(num, 14))
+                    if len(str(num)) > 16:
+                        latex_str += '...'
+        elif sig == 'irr':
+            num *= -1
+            res.remove('Entero')
+            res.remove('Racional')
+            res.append('Irracional')
+            if num == 1 or num == 4 or num == 9:
+                num += 1
+            #\sqrt{x}
+            disp = random.choice(['\sqrt', 'num'])
+            if disp == '\sqrt':
+                latex_str = '\sqrt{' + str(num) + '}'
+            else:
+                num = num ** (1/2)
+                latex_str = str(round(num, 14))
+                if len(str(num)) > 16:
+                    latex_str += '...'
+        else:
+            latex_str = str(num)
+    else:
+        latex_str = str(num)
+
+    preguntas.append(latex_str)
+    res.sort()
+    respuestas.append(res)
+    
+#Reutilizable
+with st.form("my_form"):
+   #Pregunta 1
+    st.latex(preguntas[0])
+    res_est0 = st.multiselect(
+        "1. Selecciona TODOS los que correspondan",
+        ['Natural', 'Entero', 'Racional', 'Irracional', 'Real'],
+        default=["Real"],
+    )
+    res_est0.sort()
+    
+    #Pregunta 2
+    st.latex(preguntas[1])
+    res_est1 = st.multiselect(
+        "2. Selecciona TODOS los que correspondan",
+        ['Natural', 'Entero', 'Racional', 'Irracional', 'Real'],
+        default=["Real"],
+    )
+    res_est1.sort()
+
+    #Pregunta 3
+    st.latex(preguntas[2])
+    res_est2 = st.multiselect(
+        "3. Selecciona TODOS los que correspondan",
+        ['Natural', 'Entero', 'Racional', 'Irracional', 'Real'],
+        default=["Real"],
+    )
+    res_est2.sort()
+    
+    #Pregunta 4
+    st.latex(preguntas[3])
+    res_est3 = st.multiselect(
+        "4. Selecciona TODOS los que correspondan",
+        ['Natural', 'Entero', 'Racional', 'Irracional', 'Real'],
+        default=["Real"],
+    )
+    res_est3.sort()
+
+    #Pregunta 5
+    st.latex(preguntas[4])
+    res_est4 = st.multiselect(
+        "5. Selecciona TODOS los que correspondan",
+        ['Natural', 'Entero', 'Racional', 'Irracional', 'Real'],
+        default=["Real"],
+    )
+    res_est4.sort()
+
+    logrado = st.form_submit_button('Confirmar respuestas', on_click=disable_button, disabled=st.session_state.button_disabled)
+
+#Reutilizable
+if logrado:
+    pts = 0
+    
+    #Respuesta 0
+    if respuestas[0] == res_est0:
+        st.success("1. Bravooo")
+        pts += 1
+    else:
+        mensaje_error = "1. La respuesta era: " + str(respuestas[0])
+        st.warning(mensaje_error)
+    
+    time.sleep(0.8)
+    #Respuesta 1
+    if respuestas[1] == res_est1:
+        st.success("2. Bravooo")
+        pts += 1
+    else:
+        mensaje_error = "2. La respuesta era: " + str(respuestas[1])
+        st.warning(mensaje_error)
+        
+    time.sleep(0.8)
+    #Respuesta 2
+    if respuestas[2] == res_est2:
+        st.success("3. Bravooo")
+        pts += 1
+    else:
+        mensaje_error = "3. La respuesta era: " + str(respuestas[2])
+        st.warning(mensaje_error)
+    
+    time.sleep(0.8)
+    #Respuesta 3
+    if respuestas[3] == res_est3:
+        st.success("4. Bravooo")
+        pts += 1
+    else:
+        mensaje_error = "4. La respuesta era: " + str(respuestas[3])
+        st.warning(mensaje_error)
+    
+    time.sleep(0.8)
+    #Respuesta 4
+    if respuestas[4] == res_est4:
+        st.success("5. Bravooo")
+        pts += 1
+    else:
+        mensaje_error = "5. La respuesta era: " + str(respuestas[4])
+        st.warning(mensaje_error)
+
+    std_info = session.table("primeroc.public.students").filter(col('matricula')==st.session_state.mat).collect()[0]
+
+    if pts == 5:
+        st.write(f"Felicidades por contestar todo bien. Obtienes", info[2], "punto(s) adicional.")
+        pts += info[2]
+    
+    pts = int((pts * 0.7) + (pts * 0.3 * info[2])) #1 - 6, 1 - 8, 1 - 11, 1 - 13, 2 - 16   
+    std_ac = std_info[3] + pts 
+    std_tot = std_info[4] + pts
+    
+    st.write("En esta práctica, obtuviste: **" + str(pts) + "pts.**")
+    st.write("Puntos actuales: " + str(std_tot) + "pts.")
+    
+    my_insert_stmt = """update students
+    set puntos_act = """ + str(std_ac) + """, puntos_tot = """ + str(std_tot) + """
+    WHERE matricula = """ + mat
+    session.sql(my_insert_stmt).collect()
+
+regresar = st.button("Volver a inicio")
+if regresar:
+    st.session_state.s_seed = new_seed
+    st.session_state.button_disabled = False
+    st.switch_page("pages/inicio.py")
+#Fin
+    
