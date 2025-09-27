365de566557c93fe7e07e5c4b8804b818211010a
jemiliogzz <122490063+jemiliogzz@users.noreply.github.com>
2025-09-27 16:27:38 -0600

    Update quiz_16.py

--- pages/quiz_16.py
+++ pages/quiz_16.py
@@ -73,6 +73,7 @@ for i in range (5):
     num2 = num2 + otro_num2
     
     ec = num1 * den + num2 * res * den
+    st.write(ec)
     cambio = random.randint(1, 3)
     fracc = r'\frac{' + str(ec) + '+' + str(otro_num2 * den) + 'x}{' + str(den) + '}' 
     latex_str = str(num1) + '+' + str(num2) + 'x =' + fracc
