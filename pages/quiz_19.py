cf6b77bcffc51001cd8f8c347296375029873a5e
jemiliogzz <122490063+jemiliogzz@users.noreply.github.com>
2025-09-30 08:53:35 -0600

    Create quiz_19.py

---
+++ pages/quiz_19.py
@@ -0,0 +1,214 @@
+#Reutilizable
+import streamlit as st
+import random
+import time 
+from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col
+
+if "mat" in st.session_state:
+    mat = st.session_state["mat"]
+else:
+    st.switch_page("streamlit_app.py")
+
+#st.write(st.session_state.tema)
+
+cnx = st.connection("snowflake")
+session = cnx.session()
+
+info = session.table("primeroc.public.subjects").filter(col('id_tema')==st.session_state.tema).collect()[0]
+st.title(info[1])
+st.write("Dificultad:", info[2])
+
+new_seed = random.randint(1, 10000)
+
+random.seed(st.session_state.s_seed)
+
+# Initialize session state variable
+if 'button_disabled' not in st.session_state:
+    st.session_state.button_disabled = False
+
+# Callback function to disable the button
+def disable_button():
+    st.session_state.button_disabled = True
+
+preguntas = []
+respuestas = []
+
+for i in range (5):
+    #Fin
+
+    res_pos = random.randint(1, 4 + i)
+    mul = random.randint(2, 4 + i)
+    sum = random.randint(-3 - i, 3 + i)
+    if sum == 0:
+      sum = 1
+    sum = sum * res_pos
+    fin = res_pos * mul + sum
+    latex_str = '|' + str(mul) + 'x'
+    if sum > 0:
+      latex_str += '+'
+
+    latex_str += str(sum) + '|=' + str(fin)
+    
+    res_neg = ((fin * -1) - sum) / mul
+
+    preguntas.append(latex_str)
+    respuestas.append((str(res_pos), str(res_neg))
+
+#Reutilizable
+with st.form("my_form"):
+    #Pregunta 1
+    st.latex(preguntas[0])
+    res_est00 = st.text_input("1. Ingresa la solución #1:")
+    res_est01 = st.text_input("1. Ingresa la solución #2:")
+
+    #Pregunta 2
+    st.latex(preguntas[1])
+    res_est10 = st.text_input("2. Ingresa la solución #1:")
+    res_est11 = st.text_input("2. Ingresa la solución #2:")
+
+    #Pregunta 3
+    st.latex(preguntas[2])
+    res_est20 = st.text_input("3. Ingresa la solución #1:")
+    res_est21 = st.text_input("3. Ingresa la solución #2:")
+    
+    #Pregunta 4
+    st.latex(preguntas[3])
+    res_est30 = st.text_input("4. Ingresa la solución #1:")
+    res_est31 = st.text_input("4. Ingresa la solución #2:")
+
+    #Pregunta 5
+    st.latex(preguntas[4])
+    res_est40 = st.text_input("5. Ingresa la solución #1:")
+    res_est41 = st.text_input("5. Ingresa la solución #2:")
+
+    logrado = st.form_submit_button('Confirmar respuestas', on_click=disable_button, disabled=st.session_state.button_disabled)
+
+#Reutilizable
+if logrado:
+    pts = 0
+    
+    #Respuesta 0
+    if respuestas[0][0] == res_est00.replace(" ", ""):
+      if respuestas[0][1] == res_est01.replace(" ", ""):
+        st.success("1. Bravooo")
+        pts += 1
+      else:
+        mensaje_error = "1. La respuesta era: " + str(respuestas[0][0]) + " y " + str(respuestas[0][1])
+        st.warning(mensaje_error)
+    elif respuestas[0][1] == res_est00.replace(" ", ""):
+      if respuestas[0][0] == res_est01.replace(" ", ""):
+        st.success("1. Bravooo")
+        pts += 1
+      else:
+        mensaje_error = "1. La respuesta era: " + str(respuestas[0][0]) + " y " + str(respuestas[0][1])
+        st.warning(mensaje_error)
+    else:
+        mensaje_error = "1. La respuesta era: " + str(respuestas[0][0]) + " y " + str(respuestas[0][1])
+        st.warning(mensaje_error)
+    
+    time.sleep(0.8)
+    #Respuesta 1
+    if respuestas[1][0] == res_est10.replace(" ", ""):
+      if respuestas[1][1] == res_est11.replace(" ", ""):
+        st.success("2. Bravooo")
+        pts += 1
+      else:
+        mensaje_error = "2. La respuesta era: " + str(respuestas[1][0]) + " y " + str(respuestas[1][1])
+        st.warning(mensaje_error)
+    elif respuestas[1][1] == res_est10.replace(" ", ""):
+      if respuestas[1][0] == res_est11.replace(" ", ""):
+        st.success("1. Bravooo")
+        pts += 1
+      else:
+        mensaje_error = "2. La respuesta era: " + str(respuestas[1][0]) + " y " + str(respuestas[1][1])
+        st.warning(mensaje_error)
+    else:
+        mensaje_error = "2. La respuesta era: " + str(respuestas[1][0]) + " y " + str(respuestas[1][1])
+        st.warning(mensaje_error)
+        
+    time.sleep(0.8)
+    #Respuesta 2
+    if respuestas[2][0] == res_est20.replace(" ", ""):
+      if respuestas[2][1] == res_est21.replace(" ", ""):
+        st.success("3. Bravooo")
+        pts += 1
+      else:
+        mensaje_error = "3. La respuesta era: " + str(respuestas[2][0]) + " y " + str(respuestas[2][1])
+        st.warning(mensaje_error)
+    elif respuestas[2][1] == res_est20.replace(" ", ""):
+      if respuestas[2][0] == res_est21.replace(" ", ""):
+        st.success("3. Bravooo")
+        pts += 1
+      else:
+        mensaje_error = "3. La respuesta era: " + str(respuestas[2][0]) + " y " + str(respuestas[2][1])
+        st.warning(mensaje_error)
+    else:
+        mensaje_error = "3. La respuesta era: " + str(respuestas[2][0]) + " y " + str(respuestas[2][1])
+        st.warning(mensaje_error)
+    
+    time.sleep(0.8)
+    #Respuesta 3
+    if respuestas[3][0] == res_est30.replace(" ", ""):
+      if respuestas[3][1] == res_est31.replace(" ", ""):
+        st.success("4. Bravooo")
+        pts += 1
+      else:
+        mensaje_error = "4. La respuesta era: " + str(respuestas[3][0]) + " y " + str(respuestas[3][1])
+        st.warning(mensaje_error)
+    elif respuestas[3][1] == res_est30.replace(" ", ""):
+      if respuestas[3][0] == res_est31.replace(" ", ""):
+        st.success("4. Bravooo")
+        pts += 1
+      else:
+        mensaje_error = "4. La respuesta era: " + str(respuestas[3][0]) + " y " + str(respuestas[3][1])
+        st.warning(mensaje_error)
+    else:
+        mensaje_error = "4. La respuesta era: " + str(respuestas[3][0]) + " y " + str(respuestas[3][1])
+        st.warning(mensaje_error)
+    
+    time.sleep(0.8)
+    #Respuesta 4
+    if respuestas[4][0] == res_est40.replace(" ", ""):
+      if respuestas[4][1] == res_est41.replace(" ", ""):
+        st.success("5. Bravooo")
+        pts += 1
+      else:
+        mensaje_error = "5. La respuesta era: " + str(respuestas[4][0]) + " y " + str(respuestas[4][1])
+        st.warning(mensaje_error)
+    elif respuestas[4][1] == res_est40.replace(" ", ""):
+      if respuestas[4][0] == res_est41.replace(" ", ""):
+        st.success("5. Bravooo")
+        pts += 1
+      else:
+        mensaje_error = "5. La respuesta era: " + str(respuestas[4][0]) + " y " + str(respuestas[4][1])
+        st.warning(mensaje_error)
+    else:
+        mensaje_error = "5. La respuesta era: " + str(respuestas[4][0]) + " y " + str(respuestas[4][1])
+        st.warning(mensaje_error)
+
+    std_info = session.table("primeroc.public.students").filter(col('matricula')==st.session_state.mat).collect()[0]
+
+    if pts == 5:
+        st.write(f"Felicidades por contestar todo bien. Obtienes", info[2], "punto(s) adicional.")
+        pts += info[2]
+    
+    pts = int((pts * 0.7) + (pts * 0.3 * info[2]) + 0.1) #1 - 6, 1 - 8, 1 - 11, 1 - 13, 2 - 16   
+    std_ac = std_info[3] + pts 
+    std_tot = std_info[4] + pts
+    
+    st.write("En esta práctica, obtuviste: **" + str(pts) + "pts.**")
+    st.write("Puntos actuales: " + str(std_tot) + "pts.")
+    
+    my_insert_stmt = """update students
+    set puntos_act = """ + str(std_ac) + """, puntos_tot = """ + str(std_tot) + """
+    WHERE matricula = """ + mat
+    session.sql(my_insert_stmt).collect()
+
+regresar = st.button("Volver a inicio")
+if regresar:
+    st.session_state.s_seed = new_seed
+    st.session_state.button_disabled = False
+    st.switch_page("pages/inicio.py")
+#Fin
+    
