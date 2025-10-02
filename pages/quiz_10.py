0f399c15f9f59324a2b04a26927a5157cd757424
jemiliogzz <122490063+jemiliogzz@users.noreply.github.com>
2025-10-01 20:46:37 -0600

    Update quiz_10.py

--- pages/quiz_10.py
+++ pages/quiz_10.py
@@ -9,21 +9,55 @@ if "mat" in st.session_state:
     mat = st.session_state["mat"]
 else:
     st.switch_page("streamlit_app.py")
-
-#st.write(st.session_state.tema)
-cnx = st.connection("snowflake")
-session = cnx.session()
-info = session.table("primeroc.public.subjects").filter(col('id_tema')==st.session_state.tema).collect()[0]
-st.title(info[1])
-st.write("Dificultad:", info[2])
-
 new_seed = random.randint(1, 10000)
 
-random.seed(st.session_state.s_seed)
+cnx = st.connection("snowflake")
+session = cnx.session()
+
+std_info = session.table("primeroc.public.students").filter(col('matricula')==st.session_state.mat).collect()[0]
+std_id = std_info[0]
+
+# Consultar puntos acumulados en ese tema
+query_total = f"""
+    SELECT COALESCE(SUM(PTS),0) AS TOTAL
+    FROM PRIMEROC.PUBLIC.DONE_DONE_DONE
+    WHERE ID_EST = {std_id} AND ID_TEMA = {st.session_state.tema}
+"""
+total_actual = session.sql(query_total).collect()[0]["TOTAL"]
+
+# Consultar límite del tema
+query_limite = f"""
+    SELECT LIMITE FROM PRIMEROC.PUBLIC.SUBJECTS
+    WHERE ID_TEMA = {st.session_state.tema}
+"""
+limite = session.sql(query_limite).collect()[0]["LIMITE"]
+
+# Mostrar info al alumno
+st.write(f"Has acumulado {total_actual} puntos en este tema (límite {limite}).")
+
+if total_actual > limite:
+    total_actual = limite
+st.progress(total_actual / limite)
 
 # Initialize session state variable
 if 'button_disabled' not in st.session_state:
     st.session_state.button_disabled = False
+
+if total_actual >= limite:
+    st.warning("⚠️ Ya alcanzaste el límite de puntos para este tema.")
+    time.sleep(2)
+    st.warning("⚠️ Regresa mañana.")
+    time.sleep(1)
+    st.session_state.s_seed = new_seed
+    st.session_state.button_disabled = False
+    st.switch_page("pages/inicio.py")
+#Fin
+
+info = session.table("primeroc.public.subjects").filter(col('id_tema')==st.session_state.tema).collect()[0]
+st.title(info[1])
+st.write("Dificultad:", info[2])
+
+random.seed(st.session_state.s_seed)
 
 # Callback function to disable the button
 def disable_button():
@@ -164,8 +198,6 @@ if logrado:
         mensaje_error = "5. La respuesta era: " + str(respuestas[4])
         st.warning(mensaje_error)
 
-    std_info = session.table("primeroc.public.students").filter(col('matricula')==st.session_state.mat).collect()[0]
-
     if pts == 5:
         st.write(f"Felicidades por contestar todo bien. Obtienes", info[2], "punto(s) adicional.")
         pts += info[2]
@@ -173,7 +205,6 @@ if logrado:
     pts = int((pts * 0.7) + (pts * 0.3 * info[2])) #1 - 6, 1 - 8, 1 - 11, 1 - 13, 2 - 16   
     std_ac = std_info[3] + pts 
     std_tot = std_info[4] + pts
-    std_id = std_info[0]
     
     st.write("En esta práctica, obtuviste: **" + str(pts) + "pts.**")
     st.write("Puntos Actuales: " + str(std_ac) + "pts.")
