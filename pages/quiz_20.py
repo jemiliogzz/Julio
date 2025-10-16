944da4cd133f4eecbf2f2188b627c7ef5d3df0b4
jemiliogzz <122490063+jemiliogzz@users.noreply.github.com>
2025-10-16 16:16:37 -0600

    Add quiz page with point tracking and user warnings
    This file implements a quiz page in Streamlit, including session management, point accumulation, and user feedback.

---
+++ pages/quiz_20.py
@@ -0,0 +1,79 @@
+#Reutilizable
+import streamlit as st
+import random
+from datetime import datetime
+import time 
+from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col
+
+if "mat" in st.session_state:
+    mat = st.session_state["mat"]
+else:
+    st.switch_page("streamlit_app.py")
+
+new_seed = random.randint(1, 10000)
+
+cnx = st.connection("snowflake")
+session = cnx.session()
+
+std_info = session.table("primeroc.public.students").filter(col('matricula')==st.session_state.mat).collect()[0]
+std_id = std_info[0]
+
+# Quiz en mantenimiento
+st.warning("⚠️ Quiz en mantenimiento.")
+time.sleep(2)
+st.switch_page("pages/inicio.py")
+
+# Consultar puntos acumulados en ese tema
+query_total = f"""
+    SELECT COALESCE(SUM(PTS),0) AS TOTAL
+    FROM PRIMEROC.PUBLIC.DONE_DONE_DONE
+    WHERE ID_EST = {std_id} AND ID_TEMA = {st.session_state.tema}
+"""
+total_actual = session.sql(query_total).collect()[0]["TOTAL"]
+
+# Consultar límite del tema
+query_limite = f"""
+    SELECT LIMITE FROM PRIMEROC.PUBLIC.SUBJECTS
+    WHERE ID_TEMA = {st.session_state.tema}
+"""
+limite = session.sql(query_limite).collect()[0]["LIMITE"]
+
+# Mostrar info al alumno
+st.write(f"Has acumulado {total_actual} puntos en este tema (límite {limite}).")
+
+if total_actual > limite:
+    total_actual = limite
+st.progress(total_actual / limite)
+
+# Initialize session state variable
+if 'button_disabled' not in st.session_state:
+    st.session_state.button_disabled = False
+
+if total_actual >= limite:
+    st.warning("⚠️ Ya alcanzaste el límite de puntos para este tema.")
+    time.sleep(2)
+    st.warning("⚠️ Regresa mañana.")
+    time.sleep(1)
+    st.session_state.s_seed = new_seed
+    st.session_state.button_disabled = False
+    st.switch_page("pages/inicio.py")
+
+info = session.table("primeroc.public.subjects").filter(col('id_tema')==st.session_state.tema).collect()[0]
+st.title(info[1])
+st.write("Dificultad:", info[2])
+
+random.seed(st.session_state.s_seed)
+
+# Callback function to disable the button
+def disable_button():
+    st.session_state.button_disabled = True
+
+preguntas = []
+respuestas = []
+
+for i in range (5):
+  #Fin
+  res_x = random.randint(-5, 5)
+
+  dif_problema = random.choice([1, 1, 1, 0, 0])
